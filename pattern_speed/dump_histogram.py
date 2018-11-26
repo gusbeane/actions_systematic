f0ad644415ad847f6e9076774afe433e44089afc
Gus Beane <abeane@sas.upenn.edu>
2018-11-26 16:08:36 -0500

    Update histogram dump to use ananke pa

--- pattern_speed/dump_histogram.py
+++ pattern_speed/dump_histogram.py
@@ -1,23 +1,32 @@
 import numpy as np
 import gizmo_analysis as gizmo
+from tqdm import tqdm
 
-start = 591
-end = 600
+start = 590
+end = 600 
 snap_ids = list(range(start, end+1))
+
+pa = np.array([[-0.116813976049, 0.981662058456, -0.150645603947],
+               [-0.860269343337, -0.024217138031, 0.509264358796],
+               [-0.496277293369, -0.189084989395, -0.847322674588]])
 
 rmin = 7.2
 rmax = 9.2
 zmax = 1.0
 
-for gal in ['m12i', 'm12f']:
+#for gal in ['m12i', 'm12f']:
+for gal in ['m12i']:
     sim_directory = '/mnt/ceph/users/firesims/fire2/metaldiff/' + gal + '_res7100/'
     snaps = gizmo.io.Read.read_snapshots(['star', 'gas', 'dark'], 'index', snap_ids,
                                          properties=['position', 'mass', 'velocity',
                                                      'form.scalefactor'],
                                          assign_principal_axes=True,
                                          simulation_directory=sim_directory)
-    for idx, snap in zip(snap_ids, snaps):
+    for idx, snap in tqdm(zip(snap_ids, snaps)):
+        #snap.principal_axes_vectors = snaps[0].principal_axes_vectors
+        snap.principal_axes_vectors = pa 
         for k in snap.keys():
+            snap[k].principal_axes_vectors = pa
             fout = k + '_' + gal + '_id' + str(idx) + '.npy'
             cyl_pos = snap[k].prop('host.distance.principal.cylindrical')
 
