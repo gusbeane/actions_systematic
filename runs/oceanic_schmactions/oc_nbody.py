5cba1f5315a1326352788ae3df53fa121b867155
gusbeane <abeane@sas.upenn.edu>
2018-12-02 12:42:00 -0500

    Add schmactions oceanic

---
+++ runs/oceanic_schmactions/oc_nbody.py
@@ -0,0 +1,92 @@
+import numpy as np
+from amuse.units import units
+from amuse.couple.bridge import Bridge
+from tqdm import tqdm
+
+import sys
+
+from oceanic.oc_code import oc_code
+from oceanic.options import options_reader
+
+from oceanic.gizmo_interface import gizmo_interface
+from oceanic.oceanic_io import snapshot_reader
+
+
+def evolve_cluster_in_galaxy(options_file):
+    opt = options_reader(options_file)
+
+    timestep = opt.options['timestep']  # in Myr
+    tend = opt.options['tend']  # in Myr
+    times = np.arange(0.0, tend, timestep) | units.Myr
+
+    cluster = oc_code(opt)
+    cluster_code = cluster.code
+    galaxy_code = gizmo_interface(opt)
+
+    snap_reader = snapshot_reader(opt, galaxy_code)
+
+    stars = cluster_code.particles.copy()
+
+    if opt.options['axisymmetric']:
+        import astropy.units as u
+        import gala.dynamics as gd
+        import gala.potential as gp
+
+        if opt.options['axi_Rinit'] is None \
+            or opt.options['axi_Rinit'] is None \
+            or opt.options['axi_zinit'] is None:
+            
+            pos = galaxy_code.chosen_position_z0
+            vel = galaxy_code.chosen_velocity_z0
+            stars.x += pos[0] | units.kpc
+            stars.y += pos[1] | units.kpc
+            stars.z += pos[2] | units.kpc
+            stars.vx += vel[0] | units.kms
+            stars.vy += vel[1] | units.kms
+            stars.vz += vel[2] | units.kms
+        else:
+            pos = [opt.options['axi_Rinit'], 0, 0] * u.kpc
+            vel = [0, 0, 0] * u.km/u.s
+            mw = gp.MilkyWayPotential()
+            phase = gd.PhaseSpacePosition(pos, vel)
+            vc = mw.circular_velocity(phase).to_value(u.km/u.s) | units.kms
+
+            stars = cluster_code.particles.copy()
+            stars.x += opt.options['axi_Rinit'] | units.kpc
+            stars.vy += vc
+            stars.z += opt.options['axi_zinit'] | units.kpc
+
+    else:
+        pos = galaxy_code.chosen_position_z0
+        vel = galaxy_code.chosen_velocity_z0
+        stars.x += pos[0] | units.kpc
+        stars.y += pos[1] | units.kpc
+        stars.z += pos[2] | units.kpc
+        stars.vx += vel[0] | units.kms
+        stars.vy += vel[1] | units.kms
+        stars.vz += vel[2] | units.kms
+
+    channel = stars.new_channel_to(cluster_code.particles)
+    channel.copy_attributes(["x", "y", "z", "vx", "vy", "vz"])
+
+    system = Bridge(timestep=timestep, use_threading=False)
+    system.add_system(cluster_code, (galaxy_code,))
+    system.add_system(galaxy_code)
+
+    converter = cluster_code.unit_converter
+
+    for i, t in enumerate(tqdm(times)):
+        system.evolve_model(t, timestep=timestep | units.Myr)
+
+        bound = system.particles.bound_subset(unit_converter=converter)
+        bound_com = bound.center_of_mass().value_in(units.kpc)
+
+        snap_reader.process_snapshot(system, galaxy_code, bound_com, i, t)
+
+    snap_reader.finish_sim()
+
+    cluster_code.stop()
+
+
+if __name__ == '__main__':
+    evolve_cluster_in_galaxy(sys.argv[1])
