be80a915da964cc0d330cba77e6a97eca150285d
Gus Beane <abeane@sas.upenn.edu>
2018-12-08 17:50:18 -0500

    Add init to top level directory

---
+++ init.sh
@@ -0,0 +1,4 @@
+module load gcc openmpi lib/x264/stable-aaa9aa8 ffmpeg
+module load cuda cudnn
+source ~/amuse_env/bin/activate
+export PYTHONPATH='/mnt/ceph/users/abeane/actions_systematic/runs/include':'/mnt/ceph/users/abeane/actions_systematic/runs'
