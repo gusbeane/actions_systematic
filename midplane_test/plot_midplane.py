417b7c24f76ac6cb27f4547659436aa36a2c1b59
gusbeane <abeane@sas.upenn.edu>
2019-01-01 17:33:35 -0500

    Redo midplane plot to include force midplane

--- midplane_test/plot_midplane.py
+++ midplane_test/plot_midplane.py
@@ -1,4 +1,3 @@
-import gizmo_analysis as gizmo
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -88,6 +87,15 @@ for gal,ax_col in zip(glist, ax.transpose()):
     err_vel_low = result[:,4]
     err_vel_high = result[:,5]
 
+    out_force = np.load('output/out_force_'+gal+'.npy')
+    theta_force = out_force[:,0]
+    result_force = out_force[:,1:7]
+    fit_force = out_force[:,7]
+
+    midplane_est_force = result_force[:,0]
+    err_low_force = result_force[:,1]
+    err_high_force = result_force[:,2]
+
     ax_col[0].set_xlabel(r'$\phi/\pi$')
 
     ax_col[0].text(0.05, 0.88, gal, 
@@ -99,18 +107,20 @@ for gal,ax_col in zip(glist, ax.transpose()):
     ax_col[1].set_xlim(0, 2)
 
     ax_col[0].set_ylim(-200, 200)
+    ax_col[1].set_ylim(-200, 200)
 
     ax_col[0].plot(theta/np.pi, (midplane_est-fit)*1000, c=tb_c[0])
     ax_col[0].plot(theta/np.pi, (err_low-fit)*1000, c=tb_c[0], ls='dashed', alpha=0.5)
     ax_col[0].plot(theta/np.pi, (err_high-fit)*1000, c=tb_c[0], ls='dashed', alpha=0.5)
     ax_col[0].fill_between(theta/np.pi, (err_high-fit)*1000, (err_low-fit)*1000, color=tb_c[0], alpha=0.25)
     
-    ax_col[1].plot(theta/np.pi, (midplane_vel), c=tb_c[1])
-    ax_col[1].plot(theta/np.pi, (err_vel_low), c=tb_c[1], ls='dashed', alpha=0.5)
-    ax_col[1].plot(theta/np.pi, (err_vel_high), c=tb_c[1], ls='dashed', alpha=0.5)
-    ax_col[1].fill_between(theta/np.pi, (err_vel_high), (err_vel_low), color=tb_c[1], alpha=0.25)
+    ax_col[1].plot(theta_force/np.pi, (midplane_est_force-fit_force)*1000, c=tb_c[1])
+    ax_col[1].plot(theta_force/np.pi, (err_low_force-fit_force)*1000, c=tb_c[1], ls='dashed', alpha=0.5)
+    ax_col[1].plot(theta_force/np.pi, (err_high_force-fit_force)*1000, c=tb_c[1], ls='dashed', alpha=0.5)
+    ax_col[1].fill_between(theta_force/np.pi, (err_high_force-fit_force)*1000, (err_low_force-fit_force)*1000, color=tb_c[1], alpha=0.25)
 
 ax[0][0].set_ylabel(r'$\text{midplane}\,[\,\text{pc}\,]$')
+ax[0][1].set_ylabel(r'$\text{midplane}\,[\,\text{pc}\,]$')
 
 fig.tight_layout()
 plt.savefig('midplane_fit.pdf')
