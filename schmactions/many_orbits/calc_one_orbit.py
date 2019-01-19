2e2990f06ee70a0b688ee023282b3e8d4eeb8e3a
gusbeane <abeane@sas.upenn.edu>
2019-01-19 15:18:46 -0500

    Update calc_one_orbit.py

--- schmactions/many_orbits/calc_one_orbit.py
+++ schmactions/many_orbits/calc_one_orbit.py
@@ -2,15 +2,17 @@ import sys
 sys.path.append('../')
 
 from schmactions import schmactions
+from schmactions import compute_offset_list
 
 import astropy.units as u
 import pickle
 
+nproc = 40
 init_pos = [8, 0, 0] * u.kpc
 init_vel = [0, -190, 50] * u.km/u.s
 
-zoffset = [0, 0, 100] * u.pc
-xoffset = [100, 0, 0] * u.pc
+zoffset_list = [[0, 0, z] * u.pc for z in np.arange(0, 500, 10)]
+xoffset_list = [[x, 0, 0] * u.pc for x in np.arange(0, 500, 10)]
 
 voffset = [0, 0, 0] * u.km/u.s
 
@@ -18,8 +20,8 @@ s = schmactions(init_pos, init_vel)
 
 pickle.dump(s.res, open('true_res.p', 'wb')) 
 
-zout = s.compute_actions_offset(zoffset, voffset, cadence=1)
-xout = s.compute_actions_offset(xoffset, voffset, cadence=1)
+zout = s.compute_offset_list(zoffset_list, nproc=nproc)
+xout = s.compute_offset_list(xoffset_list, nproc=nproc)
 
 pickle.dump(zout, open('zout.p', 'wb'), protocol=4)
 pickle.dump(xout, open('xout.p', 'wb'), protocol=4)
