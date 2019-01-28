83261cc5991e087ac619ba67ba4bd299deadb95f
gusbeane <abeane@sas.upenn.edu>
2019-01-28 17:33:53 -0500

    Add calc of neighborhood

---
+++ schmactions/cluster_offset_real/Rn_eval.py
@@ -0,0 +1,24 @@
+import pickle
+import numpy as np
+from astropy.table import Table
+from scipy.interpolate import interp1d
+
+mlist = np.array([ 352, 410, 112, 400, np.nan, np.nan, np.nan, np.nan, 550])
+t = Table.read('real_cluster_gc.fits', format='fits')
+name_list = t['cluster']
+
+gal_list = ['m12i', 'm12f', 'm12m']
+
+for name, mass in zip(name_list, mlist):
+    for gal in gal_list:
+        t = pickle.load(open('Rn_vs_mc_'+gal+'_'+name+'.p', 'rb'))
+        mlist = t[:,0]
+        Rn = t[:,1]
+        i = interp1d(mlist, Rn)
+        if np.isnan(mass):
+            this_Rn = np.nan
+        else:
+            this_Rn = i(mass)
+        print(name, gal, this_Rn*1000)
+
+
