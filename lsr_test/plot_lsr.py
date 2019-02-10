89adc9924a8855a50c4c816c48a7d45770fbdf53
gusbeane <abeane@sas.upenn.edu>
2019-02-10 17:33:59 -0500

    Create plot_lsr.py

---
+++ lsr_test/plot_lsr.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+from matplotlib import rc
+import matplotlib as mpl
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+mpl.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}']
+
+tb_c = ['#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f',
+        '#edc948', '#b07aa1', '#ff9da7', '#9c755f', '#bab0ac']
+
+
