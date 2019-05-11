ffb08c573470beedad648772885ee0247fa0bb57
gusbeane <abeane@sas.upenn.edu>
2019-05-11 13:09:49 -0400

    Move include out of runs folder

--- init_macbook.sh
+++ init_macbook.sh
@@ -1,4 +1,4 @@
 # module load gcc openmpi lib/x264/stable-aaa9aa8 ffmpeg
 # module load cuda cudnn
 # source ~/amuse_env/bin/activate
-export PYTHONPATH='/Users/abeane/scratch/actions_systematic/runs/include:/Users/abeane/scratch/actions_systematic/runs'
+export PYTHONPATH=/Users/abeane/scratch/actions_systematic/include:${PYTHONPATH}
